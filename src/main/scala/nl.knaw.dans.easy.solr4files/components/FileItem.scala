70da35951c84e4c095c731d838b04fdb1f33e6c2
Joke Pol <joke.pol@dans.knaw.nl>
2017-11-23 16:38:02 +0100

    log progress with info per bag, no stack trace if content not indexed

--- src/main/scala/nl.knaw.dans.easy.solr4files/components/FileItem.scala
+++ src/main/scala/nl.knaw.dans.easy.solr4files/components/FileItem.scala
@@ -36,7 +36,7 @@ case class FileItem(bag: Bag, ddm: DDM, xml: Node) extends DebugEnhancedLogging 
   }
 
   val path: String = xml.attribute("filepath").map(_.text).getOrElse("")
-  val accessibleTo: String = ( xml \ "accessRights").map(_.text).mkString match {
+  val accessibleTo: String = ( xml \ "accessibleToRights").map(_.text).mkString match {
     case "" => datasetAccessibleTo
     case s => s
   }
